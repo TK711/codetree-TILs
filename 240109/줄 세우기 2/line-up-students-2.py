7b361f96f169094abc59a8e5b74b5b1eb887ae7f
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-09 22:50:08 +0900

    Title: 줄 세우기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,18 @@
+num = int(input())
+
+class st:
+    def __init__(self, hei, wei, idx):
+        self.H = hei
+        self.W = wei
+        self.I = idx
+
+stds = []
+
+for idx, i in enumerate(range(num), start = 1):
+    hei, wei = tuple(input().split())
+    stds.append(st(int(hei),int(wei),idx))
+
+stds.sort(key = lambda x: (x.W,-x.H))
+
+for x in stds:
+    print("%d %d %d" % (x.H,x.W,x.I))
