27030c307af5e56e49689c32a408993ffc4ac13c
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-10 23:03:32 +0900

    Title: DateTime to DateTime, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240110/DateTime to DateTime/datetime-to-datetime.py
+++ 240110/DateTime to DateTime/datetime-to-datetime.py
@@ -1,7 +1,11 @@
 d, h, m = input().split()
 d, h, m = int(d), int(h), int(m)
 
-if d <=11 and h<11 and m<11:
+if d <11:
+    print(-1)
+elif d == 11 and h<11:
+    print(-1)
+elif d == 11 and h==11 and c<11:
     print(-1)
 else:
     start = 11* 24* 60 + 11*60 + 11
