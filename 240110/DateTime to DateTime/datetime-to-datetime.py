3283d10c80c1d9377e2feb36f97053407e7caea6
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-10 22:59:53 +0900

    Title: DateTime to DateTime, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240110/DateTime to DateTime/datetime-to-datetime.py
+++ 240110/DateTime to DateTime/datetime-to-datetime.py
@@ -1,7 +1,10 @@
 d, h, m = input().split()
 d, h, m = int(d), int(h), int(m)
 
-start = 11* 24* 60 + 11*60 + 11
-passed = d* 24* 60 + h*60 + m
-            
-print(passed-start)
+if d <=11 and h<11 and m<11:
+    print(-1)
+else:
+    start = 11* 24* 60 + 11*60 + 11
+    passed = d* 24* 60 + h*60 + m
+                
+    print(passed-start)
