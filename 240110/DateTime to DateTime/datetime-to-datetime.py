75b315671c4dfabc4f489d80b23c121c0e1bc613
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-10 23:07:18 +0900

    Title: DateTime to DateTime, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240110/DateTime to DateTime/datetime-to-datetime.py
+++ 240110/DateTime to DateTime/datetime-to-datetime.py
@@ -1,14 +1,9 @@
 d, h, m = input().split()
 d, h, m = int(d), int(h), int(m)
 
-if d <11:
-    print(-1)
-elif d == 11 and h<11:
-    print(-1)
-elif d == 11 and h==11 and m<11:
-    print(-1)
+tot = d* 24* 60 + h*60 + m - (11* 24* 60 + 11*60 + 11)
+
+if tot<0:
+    print("-1")
 else:
-    start = 11* 24* 60 + 11*60 + 11
-    passed = d* 24* 60 + h*60 + m
-                
-    print(passed-start)
+    print(tot)
