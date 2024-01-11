9edda7d16204476a724891a4f9b71649689d5334
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-11 16:23:02 +0900

    Title: 그 요일은, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/그 요일은/the-day-of-the-day.py
+++ 240111/그 요일은/the-day-of-the-day.py
@@ -1,37 +1,44 @@
 m1, d1, m2, d2 = input().split()
-m1, d1, m2, d2 = int(m1),int(d1),int(m2),int(d2)
+m1, d1, m2, d2 = int(m1), int(d1), int(m2), int(d2)
 mat_day = input()
 
-#print(m1, d1, m2, d2)
-
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 name_of_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-#print(num_of_days[m1])
-elasped_day = 0
 
-# m1,d1 = monday
-# m2, d2 = ?
-#m1 d1 > m2 d2
+count = 0
+today = 0
 
-while not (m1 == m2 and d1 == d2):
-    if m1 < m2 or (m1 == m2 and d1 < d2):
+# if m1 > m2 or (m1 == m2 and d1 > d2):  # if m1d1 is later than m2d2
+#     m1, m2 = m2, m1
+#     d1, d2 = d2, d1
+#     today = (today - (d1 % 7) + 7) % 7  # adjust the starting day
+
+if not (m1 > m2 or (m1 == m2 and d1 > d2)):  # if m1d1 is later than m2d2
+    while not (m1 == m2 and d1 == d2):
+        if name_of_days[today] == mat_day:
+            count += 1
         d1 += 1
-        elasped_day += 1
+        today = (today + 1) % 7
+
         if d1 > num_of_days[m1]:
             m1 += 1
             d1 = 1
-    else:
+
+    if name_of_days[today] == mat_day:
+        count += 1
+else:
+    while not (m1 == m2 and d1 == d2):
+        #print("m1, d1, m2, d2",m1, d1, m2, d2)
+        if name_of_days[today] == mat_day:
+            count += 1
         d1 -= 1
-        elasped_day -= 1
+        today = (today + 1) % 7
+
         if d1 < 1:
             m1 -= 1
             d1 = num_of_days[m1]
 
-elasped_day = abs(elasped_day)
-day = elasped_day//7
-remain = elasped_day%7
+    if name_of_days[today] == mat_day:
+        count += 1
 
-if day <= remain:
-    day += 1
-
-print(day)
+print(count)
