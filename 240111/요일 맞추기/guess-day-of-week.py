fa45815a6bd181a7d9ba786571291a307a4baa43
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-11 09:13:33 +0900

    Title: 요일 맞추기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240111/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,45 @@
+m1, d1, m2, d2 = input().split()
+m1, d1, m2, d2 = int(m1),int(d1),int(m2),int(d2)
+
+#print(m1, d1, m2, d2)
+
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+name_of_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+#print(num_of_days[m1])
+elasped_day = 0
+
+# m1,d1 = monday
+# m2, d2 = ?
+#m1 d1 > m2 d2
+
+if m1 <= m2 and d1 <= d2:
+    while True:
+        if m1 == m2 and d1 == d2:
+            break
+        else:
+            d1 += 1
+            elasped_day += 1
+        # month calculation had difficulty
+
+        if d1 >= num_of_days[m1]:
+            m1 += 1
+            d1 = 0
+elif m1 > m2 or m1 == m2 and d1 >= d2:
+    while True:
+        if m1 == m2 and d1 == d2:
+            break
+        else:
+            d1 -= 1
+            elasped_day += 1
+        # month calculation had difficulty
+
+        if d1 <= 0:
+            m1 -= 1
+            d1 = num_of_days[m1]
+    elasped_day += 5
+#elif m1 < m2 or m1 == m2 and d1 < d2:
+
+    #1  2  3  4  5  6  7
+    #8  9 10 11 12 13 14
+
+print(name_of_days[elasped_day%7])
