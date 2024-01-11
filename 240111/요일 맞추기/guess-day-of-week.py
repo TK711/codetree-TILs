7076ace86c82872f609e5b3a63d86d7ce8f280ca
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-11 10:51:54 +0900

    Title: 요일 맞추기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/요일 맞추기/guess-day-of-week.py
+++ 240111/요일 맞추기/guess-day-of-week.py
@@ -18,7 +18,7 @@ while not (m1 == m2 and d1 == d2):
     if m1 < m2 or (m1 == m2 and d1 < d2):
         d1 += 1
         elasped_day += 1
-        if d1 > num_of_days[m1-1]:
+        if d1 > num_of_days[m1]:
             m1 += 1
             d1 = 1
     else:
