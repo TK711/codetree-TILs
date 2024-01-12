540d1c2e8fb825e277dfb86a1ac805ce176297d2
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-12 09:41:30 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240112/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,25 @@
+num = int(input())
+
+lst = [0] * 100
+lst2 = []
+mx = 0
+
+for i in range(num):
+    a1,a2 = input().split()
+    a1,a2 = int(a1),int(a2)
+
+    lst2.append([a1,a2])
+    if a1 == a2:
+        lst2.append([a1,a1+1])
+
+# 각 리스트내 범위 돌며 체크
+# 오프셋 절댓값을 추가한다
+
+for i in lst2:
+    for j in range(int(i[0]),int(i[1]+1)):
+        lst[j] += 1
+
+for i in range(1,len(lst)):
+    if mx < lst[i]:
+        mx = lst[i]
+print(mx)
