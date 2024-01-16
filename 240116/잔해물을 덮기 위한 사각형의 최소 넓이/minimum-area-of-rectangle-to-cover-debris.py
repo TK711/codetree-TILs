76e8f632d0c9d34443258bf625464771d09301e0
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-16 22:19:43 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 179ms, Memory: 55MB, Status: Wrong Answer - CodeTree

---
+++ 240116/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,27 @@
+rec1 = list(input().split())
+rec2 = list(input().split())
+
+cnt = 0
+lst3 = []
+# change 21 to 2001
+cor = list([0]*2001 for _ in range(2001))
+
+for x1 in range(int(rec1[0])+1000,int(rec1[2])+1001):
+    for y1 in range(int(rec1[1])+1000, int(rec1[3])+1001):
+        cor[x1][y1] = 1
+
+for x2 in range(int(rec2[0])+1000,int(rec2[2])+1001):
+    for y2 in range(int(rec2[1])+1000, int(rec2[3])+1001):
+        cor[x2][y2] = 0
+
+for x3 in range(2001):
+    for y3 in range(2001):
+        if cor[x3][y3] == 1:
+            lst3.append([x3,y3])
+
+lst3_x = sorted(lst3,key=lambda x: x[0])
+lst3_y = sorted(lst3,key=lambda x: x[1])
+# #print(lst3_x)
+# print(max(lst3_x),min(lst3_x))
+#print(max(lst3_y)[1],min(lst3_y)[1])
+print((max(lst3_x)[0]-min(lst3_x)[0]) * (max(lst3_y)[1]-min(lst3_y)[1]))
