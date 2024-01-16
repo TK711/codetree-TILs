0b3e832beec8bcb452578658fae9bf9e4e013369
TK711 <147051227+TK711@users.noreply.github.com>
2024-01-16 23:24:26 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 565ms, Memory: 162MB, Status: Passed - CodeTree

--- 240116/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240116/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -6,12 +6,12 @@ lst3 = []
 # change 21 to 2001
 cor = list([0]*2001 for _ in range(2001))
 
-for x1 in range(int(rec1[0])+1000,int(rec1[2])+1001):
-    for y1 in range(int(rec1[1])+1000, int(rec1[3])+1001):
+for x1 in range(int(rec1[0])+1000,int(rec1[2])+1000):
+    for y1 in range(int(rec1[1])+1000, int(rec1[3])+1000):
         cor[x1][y1] = 1
 
-for x2 in range(int(rec2[0])+1000,int(rec2[2])+1001):
-    for y2 in range(int(rec2[1])+1000, int(rec2[3])+1001):
+for x2 in range(int(rec2[0])+1000,int(rec2[2])+1000):
+    for y2 in range(int(rec2[1])+1000, int(rec2[3])+1000):
         cor[x2][y2] = 0
 
 for x3 in range(2001):
@@ -19,6 +19,6 @@ for x3 in range(2001):
         if cor[x3][y3] == 1:
             lst3.append([x3,y3])
 if lst3 != []:
-    print((max(lst3, key=lambda x: x[0])[0] - min(lst3, key=lambda x: x[0])[0]) * (max(lst3, key=lambda x: x[1])[1] - min(lst3, key=lambda x: x[1])[1]))
+    print((max(lst3, key=lambda x: x[0])[0] - min(lst3, key=lambda x: x[0])[0]+1) * (max(lst3, key=lambda x: x[1])[1] - min(lst3, key=lambda x: x[1])[1]+1))
 else:
     print(0)
