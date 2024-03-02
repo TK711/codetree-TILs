a9284db2be25038a575e7a2ae73a7ea1ea44c645
TK711 <147051227+TK711@users.noreply.github.com>
2024-03-02 23:33:57 +0900

    Title: 1차원 폭발 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240302/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,36 @@
+# 변수 선언 및 입력:
+
+N, M = map(int,input().split())
+lst = []
+for i in range(N):
+    lst.append(int(input()))
+while True:
+    prev = -1
+    cnt = 1
+    lst2 = []
+    flag = True
+    for idx, i in enumerate(lst):
+        if not i == prev:
+            if cnt >= M:
+                flag = False
+                for _ in range(cnt):
+                    lst2.pop()
+            cnt = 1
+            prev = i
+        else:
+            cnt += 1
+        lst2.append(i)
+    if cnt >= M:
+        flag = False
+        for _ in range(cnt):
+            lst2.pop()
+    lst = lst2
+    if flag == True:
+        break
+
+if lst:
+    print(len(lst))
+    for i in lst:
+        print(i)
+else:
+    print(0)
