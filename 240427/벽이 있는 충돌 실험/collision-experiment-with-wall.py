984e6c20dc79295fb13895c30d1aba9b318a8195
TK711 <147051227+TK711@users.noreply.github.com>
2024-04-27 14:42:32 +0900

    Title: 벽이 있는 충돌 실험, Time: 130ms, Memory: 72MB, Status: Wrong Answer - CodeTree

--- 240427/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240427/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,24 +1,16 @@
 for tc in range(1,int(input())+1):
-    direc = {'U':[1,0],'D':[-1,0],'R':[0,1],'L':[0,-1]}
+    direc = {'D':[1,0],'U':[-1,0],'R':[0,1],'L':[0,-1]}
     n, m = map(int,input().split())
 
     mp = [[0] * (n+1) for _ in range(n+1)]
 
-    # print(n,m)
-    # print()
-    # for k in mp:
-    #     print(k)
-    # print()
     lst = ['N']
     for k in range(1,m+1):
-        # print(k)
-        x, y, dr = list(input().split())
+        y, x, dr = list(input().split())
         x, y = int(x), int(y)
-        # print(x,y,dr)
         mp[y][x] = k
         lst.append(dr)
-    # for k in mp:
-    #     print(k)
+
 
     for _ in range(2*m+1):
         new_mp = [[0] * (n+1) for _ in range(n+1)]
@@ -26,7 +18,6 @@ for tc in range(1,int(input())+1):
             for j in range(n + 1):
                 if mp[i][j] > 0:
                     num = mp[i][j]
-                    # dire = lst[num]
                     if lst[num]=='U':
                         new_y = i + direc['U'][0]
                         new_x = j + direc['U'][1]
@@ -58,13 +49,6 @@ for tc in range(1,int(input())+1):
         for i in range(n+1):
             for j in range(n + 1):
                 mp[i][j] = new_mp[i][j]
-    #     print()
-    #     for k in mp:
-    #         print(k)
-    # print()
-    # print('fin')
-    # for k in mp:
-    #     print(k)
 
     cnt = 0
 
