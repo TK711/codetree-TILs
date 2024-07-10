af6f7bbe4b800d453d0369126c0ed56dffc45ae0
TK711 <147051227+TK711@users.noreply.github.com>
2024-07-10 20:23:55 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240710/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,28 @@
+num = int(input())
+
+mx = 100
+pivot = 50
+lst = [[0] for _ in range(mx *2 +1)]
+
+for _ in range(num):
+    dist, dire = input().split()
+    dist = int(dist)
+    for checker in range(dist):
+        if dire == "L":
+            lst[pivot] = 1
+            if checker != dist - 1:
+                pivot -= 1
+        else:
+            lst[pivot] = 2
+            if checker != dist - 1:
+                pivot += 1
+
+total_white = 0
+total_black = 0
+
+for a in lst:
+    if a == 1:
+        total_white += 1
+    elif a == 2:
+        total_black += 1
+print(total_white, total_black)
