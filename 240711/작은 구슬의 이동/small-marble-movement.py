dca7b1250dfcfa5504bc9578f6c8204250ae8da6
TK711 <147051227+TK711@users.noreply.github.com>
2024-07-11 20:26:07 +0900

    Title: 작은 구슬의 이동, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,21 @@
+n, t = list(map(int, input().split()))
+r, c, d = input().split()
+r = int(r) - 1
+c = int(c) - 1
+
+lst = [[0 for _ in range(n)] for _ in range(n) ]
+
+dx = [0,1,-1,0]
+dy = [1,0,0,-1]
+dire = {'U':0,'R':1,'L':2,'D':3}
+direction = dire[d]
+for _ in range(t):    
+    nc, nr = c + dx[direction], r + dy[direction]
+    if nr < 0 or nr >= n:
+        direction = abs(3-direction)
+    elif nc < 0 or nc >= n:
+        direction = abs(3-direction)
+    else:
+        r, c = nr,nc
+
+print(r+1,c+1)
