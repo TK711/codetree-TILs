0a005504f7e6784260c757c1edd4d0e686c39f6b
TK711 <147051227+TK711@users.noreply.github.com>
2024-11-27 22:06:42 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241127/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
+++ 241127/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -3,31 +3,42 @@
 
 using namespace std;
 #define First ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
-vector<int> v;
-vector<int> ans;
-int K,N;
 
-void choose(int cur){
-    if(cur == N+1){
-        for(int i=0; i<ans.size(); i++){
+vector<int> ans;
+int K, N;
+
+void choose(int cur) {
+    if (ans.size() == N) { 
+        for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << ' ';
-        }cout << "\n";
+        }
+        cout << "\n";
         return;
     }
-    for(int i=1; i<=N; i++){
+
+    for (int i = 1; i <= K; i++) {
         ans.push_back(i);
         choose(cur + 1);
-        ans.pop_back();
+        ans.pop_back(); 
     }
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     First
-    cin >> K >> N;
-    choose(1);
-
-
-
+    cin >> K >> N; 
+    choose(0); 
     return 0;
-}
+}2019181721222324252627282930
+        choose(cur + 1);
+        ans.pop_back(); 
+    \}
+\}
+
+int main() {
+    First
+    cin >> K >> N; 
+    choose(0); 
+    return 0;
+
+10void choose(int cur) {
+$0
